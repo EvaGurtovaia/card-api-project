f4cf704b60b9e96281e568e431839a6fb20f284e
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-14 19:30:33 -0600

    Working on fetching new cards with AJAX

--- card-api-project/src/Deck.js
+++ card-api-project/src/Deck.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 import axios from "axios";
-const apiUrl = "https://deckofcardsapi.com/api/deck/new/shuffle";
+const baseApiUrl = "https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
     constructor(props) {
         super(props);
         this.state = { deck: null };
+        this.getCard = this.getCard.bind(this);
     }
 
     async componentDidMount() {
-        let deck = await axios.get(apiUrl);
+        let deck = await axios.get(`${baseApiUrl}/new/shuffle`);
         this.setState({ deck: deck.data });
+    }
+    async getCard() {
+        let id = this.state.deck.deck_id;
+        let cardUrl = `${baseApiUrl}/${id}/draw/`;
+        let cardRes = await axios.get(cardUrl);
+        console.log(cardRes);
     }
     render() {
         return (
             <div>
                 <h1>Card Dealer</h1>
+                <button onClick={this.getCard}>Get card!</button>
             </div>
         );
     }
